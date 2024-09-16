0524b9b04f3064dc654457968ad940cf0ccf23e0
Anastasia Korzh <anakor192@ihmeducation.se>
2024-09-16 14:48:02 +0200

    cretive exercise

---
+++ P9Functions/js/app.js
@@ -0,0 +1,32 @@
+//Using those two prints, make a character:
+//walk three steps forward
+//then turn around and walk three steps forward
+//then turn left an walk one step forward
+
+concole.log ("Move Forward.");
+console.log ("Turn Righy");
+
+
+
+function stepForward() :{ no usage new
+  concole.log ("Move Forward.");
+{
+  function turnRight () { no usage new
+    console.log("Turn Right");
+}
+
+function turnLeft () { no usage new}
+  turnRight ();
+  turnRight()
+  turnRight()
+
+  function walkThreeSteps () {
+    stepForward();
+    stepForward();
+    stepForward();
+  }
+
+function turnAround () :void {
+    turnRight()
+}
+
