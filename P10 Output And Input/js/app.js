e3948710d4cc9cdac32e566f721b71601dd82ce2
Anastasia Korzh <anakor192@ihmeducation.se>
2024-09-17 09:42:19 +0200

    exercise

---
+++ P10 Output And Input/js/app.js
@@ -0,0 +1,42 @@
+function sayManyThings () {
+  console.log
+}
+
+
+
+// Function with no input and an output
+function getBankBalance():number {
+  return 20;
+}
+
+concole.log (getBankBalance());
+
+
+//funktion with input(s) and no output
+function addOne(inputNumber): void {
+  inputNumber += 1;
+  console.log(inputNumber)
+}
+
+//calling
+
+addOne(inputNumber:8);
+
+//Function with Input (s) and an output
+
+function square(numberToSquare) {
+
+  return  numberToSquare * numberToSquare;
+}
+
+//Calling
+let squaredNumber = square (numberToSquare:15);
+console.log(squaredNumber);
+
+console.log(square(numberToSquare:15));
+
+
+function doubleNumber(numberToDouble) {
+  console.log(number
+}
+
