1b61f86d5f9915214840619d19b4be032d26fad9
Anastasia Korzh <anakor192@ihmeducation.se>
2024-09-19 09:31:09 +0200

    exercise creative

--- P13-HTML DOM/js/app.js
+++ P13-HTML DOM/js/app.js
@@ -10,16 +10,22 @@ const headerText5 = document.getElementById("header5");
 
 
 // Take headerText5 and change it to "THIS IS MA WEBSITE"
-headerText5.innerHTML = multiplyNumber(5);
+headerText5.innerHTML = multiplyBySelf(5);
 
 
 // Take headerText5, access its style, access its color & change it
 headerText5.style.color = "pink";
 
 
-function multiplyNumber (numberToMultiply) {
+function multiplyBySelf (numberToMultiply) {
   return numberToMultiply * numberToMultiply;
 }
 
+// Reference "text0" and assign it to the variable called "textField0"
+const textField0 = document.getElementById("text0");
+// TAKE textField0 and change it to "Ben"
+textField0.innerHTML = getRandomInt(20);
 
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
